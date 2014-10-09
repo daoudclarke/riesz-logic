da4d2e291d77b5e0067658531a48fa16065f6338
Daoud Clarke <daoud.clarke@gmail.com>
2014-10-09 16:37:42 +0100

    Start reformatting for IEEE

--- code/proofparser/proof2tex.py
+++ code/proofparser/proof2tex.py
@@ -91,10 +91,10 @@ def to_latex(interpretation):
                 except KeyError:
                     new_line += line[i]
                 i += 1
-        tag = "(%d)" % number
+        tag = "%d." % number
         uses_labels = map(str, uses_labels)
-        uses = '\\text{(%s)}' % ', '.join(uses_labels)
-        result = '& ' + tag + ' && ' + new_line + ' & ' + uses + r'\\'
+        uses = '\\text{by %s}' % ', '.join(uses_labels)
+        result = '' + tag + ' & ' + new_line + ' \hfill ' + uses + r'\\'
         yield result
 
 if __name__ == "__main__":
